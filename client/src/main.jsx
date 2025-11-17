40e51df5e58b5894a6e4aecf314ef26cf9694233
srishylam burla <srishylamburla1@gmail.com>
2025-11-17 15:09:44 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,13 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Provider store={store}>
