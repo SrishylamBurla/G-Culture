b69a0a424aca0ff080f894b73b78b3d9dbcbb840
srishylam burla <srishylamburla1@gmail.com>
2025-11-18 20:40:17 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"/",
   build: {
     chunkSizeWarningLimit: 2000, // increases limit to 2MB
   }
