b31bda7faa73a5fcd04da88ff8666cdac1747f16
Srishylam Burla <srishylamburla1@gmail.com>
2026-08-14 15:10:41 +0530

    updated

--- client/src/components/SmoothScroll.jsx
+++ client/src/components/SmoothScroll.jsx
@@ -3,16 +3,21 @@ import Lenis from "lenis";
 
 export default function SmoothScroll() {
   useEffect(() => {
+    /*
+     * Don't run Lenis on mobile.
+     * Native touch scrolling is smoother and more reliable
+     * with Swiper.
+     */
+    if (window.innerWidth <= 767) {
+      return;
+    }
+
     const lenis = new Lenis({
       duration: 0.8,
       lerp: 0.1,
       smoothWheel: true,
-
-      // Let the browser/Swiper handle touch.
       syncTouch: false,
-
       wheelMultiplier: 1,
-      touchMultiplier: 1,
     });
 
     let rafId;
